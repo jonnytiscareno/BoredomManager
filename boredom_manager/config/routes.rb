7bc05011358ec19b705b2220e3e84aa96c1e5444
Jonny Tiscareno <jonnytiscareno@berkeley.edu>
2018-04-23 13:47:27 -0700

    locations MVC

--- boredom_manager/config/routes.rb
+++ boredom_manager/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root 'home#index'
   resources :activities
-
+  resources :locations
   get '/activities/show', to: 'activities#show'
   get '/search', to: 'activities#get'
   # get '/users/sign_out', to: 'sessions#destroy'
--- boredom_manager/db/schema.rb
+++ boredom_manager/db/schema.rb
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 20180423013711) do
     t.integer "user_id"
   end
 
+  create_table "locations", force: :cascade do |t|
+    t.string "address"
+    t.float "latitude"
+    t.float "longitude"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
