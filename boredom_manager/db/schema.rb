8201354e7bb59e25366a0fa7b73bd0b5be4faa57
Jonny Tiscareno <jonnytiscareno@berkeley.edu>
2018-04-24 20:52:42 -0700

    update locations model

--- boredom_manager/db/schema.rb
+++ boredom_manager/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180423013711) do
+ActiveRecord::Schema.define(version: 20180425034255) do
 
   create_table "activities", force: :cascade do |t|
     t.string "name"
@@ -25,8 +25,8 @@ ActiveRecord::Schema.define(version: 20180423013711) do
     t.string "address"
     t.float "latitude"
     t.float "longitude"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
